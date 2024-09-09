1e592910463f0ee6628f0d781af364748298e6cd
inspjosh150607 <150380172+inspjosh150607@users.noreply.github.com>
2024-09-09 12:28:30 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
+global.ownernomer = "2349023371564"
 global.ownername = "Precious Ayomide👑"
 global.ytname = "YT: Prexzyvilla"
-global.socialm = "GitHub: Prexzybooster"
-global.location = "Nigeria, Osun, Osogbo"
+global.socialm = "GitHub: inspjosh150607"
+global.location = "Nigeria, Lagos, Ikeja"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
-global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '234023371564'  //creator number
+global.ownername = 'Ayomide👑' //owner name
+global.botname = 'MIDE ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +263717750825'
+global.author = 'mide👑\n\nContact: +2349023371564'
 
 //console view/theme
 global.themeemoji = '🪀'
